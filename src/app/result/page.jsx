e1603bc8f660b9fc67c61ecf5295a050c40a1650
Navaneeth210805 <navaneethakrishnan2108@gmail.com>
2024-06-15 18:59:25 +0530

    Quiz app

--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -25,7 +25,7 @@ function Page() {
             </button>
           </Link>
         </div>
-        <a href="https://github.com/AdityaSaiKonduri/Tech-Affairs-2024-Tasks" target='_blank' className='underline-none w-auto flex items-center justify-center gap-4 hover:text-yellow-600'>
+        <a href="https://github.com/Navaneeth210805/quizapp" target='_blank' className='underline-none w-auto flex items-center justify-center gap-4 hover:text-yellow-600'>
         <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961" />
         </svg>
